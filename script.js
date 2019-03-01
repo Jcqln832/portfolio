e3acf5080c3ee4fd4ded9ae56751550e0df9ccf5
Jackie Galarza <jcqlnray@gmail.com>
2019-03-01 13:46:22 -0500

    interativity updates

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 'use strict';
 
-$('button').click(function(){
+$('button, .nav-link').click(function(){
     $(".nav-list").toggleClass("active");
- });
+});
+
+
+ /* adapted from https://www.taniarascia.com/smooth-scroll-to-id-with-jquery/ */
+
+ $("a[href^='#']").click(function(e) {
+    e.preventDefault();
+    var navHeight = $("nav").height()
+	
+	var position = $($(this).attr("href")).offset().top - navHeight;
+
+	$("body, html").animate({
+		scrollTop: position
+	}, 300, 'swing' );
+});
+
+// let nodeArray = [
+//     $('.about-content'),
+//     $(".headshot")
+// ];
+ScrollReveal().reveal('.contact-content', { easing: 'ease-in-out', duration: 550, delay: 200 });
